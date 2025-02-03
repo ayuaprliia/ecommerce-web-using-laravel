83afeb4428499464120c0bd0d077f595f29df2e5
Ayu Aprilia <114905113+ayuaprliia@users.noreply.github.com>
2025-02-04 04:04:21 +0800

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -13,8 +13,6 @@ class CartController extends Controller
     $itemId = $request->input('itemId');
     $quantity = $request->input('quantity');
 
-    // Lakukan validasi atau operasi lain sesuai kebutuhan
-
     // Simpan ke keranjang
     $cartItem = new Cart();
     $cartItem->product_id = $itemId;
@@ -27,11 +25,11 @@ class CartController extends Controller
 
     public function updateCart(Request $request)
     {
-        // Validasi input di sini jika diperlukan
+        // Validasi input
         $itemId = $request->input('itemId');
         $quantity = $request->input('quantity');
 
-        // Perbarui jumlah dan total harga di sini sesuai dengan kebutuhan Anda
+        // Update jumlah dan total harga 
         $cartItem = Cart::find($itemId);
 
         // Perbarui jumlah
@@ -41,7 +39,7 @@ class CartController extends Controller
         // Hitung total harga
         $itemTotal = $cartItem->price * $quantity;
 
-        // Hitung total harga semua item dalam cart
+        //total harga semua item dalam cart
         $cartItems = Cart::all();
         $cartTotal = $cartItems->sum(function ($item) {
             return $item->price * $item->quantity;
